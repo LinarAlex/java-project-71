b678263ede2e46f94efcad527ad71aa230acfcb9
LinarAlex <linch.nn@gmail.com>
2023-11-03 00:55:55 +0300

    third step of project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import java.util.concurrent.Callable;
 
 public class App {
+
+    @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+            description = "Compares two configuration files and shows a difference.")
+    static class CheckSum implements Callable<Integer> {
+        @Override
+        public Integer call() {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int exitCode = new CommandLine(new CheckSum()).execute(args);
+        System.exit(exitCode);
     }
 }
