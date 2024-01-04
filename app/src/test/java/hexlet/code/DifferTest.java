37053b0307c43f5ddf9458e519dbb77f4e5a07c2
LinarAlex <linch.nn@gmail.com>
2024-01-04 16:07:37 +0300

    fifth refactoring

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -15,6 +15,10 @@ public class DifferTest {
     private static String resultJson;
     private static String resultPlain;
     private static String resultStylish;
+    private static String file1;
+    private static String file2;
+    private static String file3;
+    private static String file4;
 
     private static Path getFilePath(String fileName) {
         return Paths.get("src", "test", "resources", fileName)
@@ -30,69 +34,49 @@ public class DifferTest {
         resultJson = getResult("testJson.json");
         resultPlain = getResult("testPlain");
         resultStylish  = getResult("testStylish");
+        file1 = getFilePath("step81.json").toString();
+        file2 = getFilePath("step82.json").toString();
+        file3 = getFilePath("step81.yml").toString();
+        file4 = getFilePath("step82.yml").toString();
     }
 
     @Test
     public void test1() throws Exception {
-        String expected = resultStylish;
-        String path1 = String.valueOf(getFilePath("step81.json"));
-        String path2 = String.valueOf(getFilePath("step82.json"));
-        assertEquals(expected, Differ.generate(path1, path2));
+        assertEquals(resultStylish.trim(), Differ.generate(file1, file2));
     }
 
     @Test
     public void test2() throws Exception {
-        String expected = resultStylish;
-        String path3 = String.valueOf(getFilePath("step81.yml"));
-        String path4 = String.valueOf(getFilePath("step82.yml"));
-        assertEquals(expected, Differ.generate(path3, path4));
+        assertEquals(resultStylish, Differ.generate(file3, file4));
     }
 
     @Test
     public void testStylish1() throws Exception {
-        String expected = resultStylish;
-        String path1 = String.valueOf(getFilePath("step81.json"));
-        String path2 = String.valueOf(getFilePath("step82.json"));
-        assertEquals(expected, Differ.generate(path1, path2, "stylish"));
+        assertEquals(resultStylish.trim(), Differ.generate(file1, file2, "stylish"));
     }
 
     @Test
     public void testStylish2() throws Exception {
-        String expected = resultStylish;
-        String path3 = String.valueOf(getFilePath("step81.yml"));
-        String path4 = String.valueOf(getFilePath("step82.yml"));
-        assertEquals(expected, Differ.generate(path3, path4, "stylish"));
+        assertEquals(resultStylish, Differ.generate(file3, file4, "stylish"));
     }
 
     @Test
     public void testPlain1() throws Exception {
-        String expected = resultPlain;
-        String path1 = String.valueOf(getFilePath("step81.json"));
-        String path2 = String.valueOf(getFilePath("step82.json"));
-        assertEquals(expected, Differ.generate(path1, path2, "plain"));
+        assertEquals(resultPlain, Differ.generate(file1, file2, "plain"));
     }
 
     @Test
     public void testPlain2() throws Exception {
-        String expected = resultPlain;
-        String path3 = String.valueOf(getFilePath("step81.yml"));
-        String path4 = String.valueOf(getFilePath("step82.yml"));
-        assertEquals(expected, Differ.generate(path3, path4, "plain"));
+        assertEquals(resultPlain, Differ.generate(file3, file4, "plain"));
     }
 
     @Test
     public void testJson1() throws Exception {
-        String expected = resultJson;
-        String path1 = String.valueOf(getFilePath("step81.json"));
-        String path2 = String.valueOf(getFilePath("step82.json"));
-        JSONAssert.assertEquals(expected, Differ.generate(path1, path2, "json"), false);
+        JSONAssert.assertEquals(resultJson, Differ.generate(file1, file2, "json"), false);
     }
 
     @Test
     public void testJson2() throws Exception {
-        String expected = resultJson;
-        String path3 = String.valueOf(getFilePath("step81.yml"));
-        String path4 = String.valueOf(getFilePath("step82.yml"));
-        JSONAssert.assertEquals(expected, Differ.generate(path3, path4, "json"), false);
+        JSONAssert.assertEquals(resultJson, Differ.generate(file3, file4, "json"), false);
     }
 }
